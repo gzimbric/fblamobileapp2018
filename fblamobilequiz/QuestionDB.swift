543ac59d03518b077a7b70f4fb729315458c9899
gzimbric <gabe@zimbri.cc>
2019-03-04 18:19:55 -0600

    update
    -app backbone completed
    -integrated with firebase
    -login and support systems completed
    -quiz functions completed
    -ui in progress
    -commenting in progress
    -few bugs need to be fixed

---
+++ fblamobilequiz/QuestionDB.swift
@@ -0,0 +1,41 @@
+//
+//  QuestionDB.swift
+//  fblamobilequiz
+//
+//  Created by Gabe Zimbric on 12/6/18.
+//  Copyright © 2018 Gabe Zimbric. All rights reserved.
+//
+
+import Foundation
+
+class QuestionDB {
+    var list = [Question]()
+    
+    init() {
+        list.append(Question(questionString: "type question here", correctAnswer: true))
+        
+        list.append(Question(questionString: "type question here", correctAnswer: false))
+        
+        list.append(Question(questionString: "type question here", correctAnswer: true))
+        
+        list.append(Question(questionString: "type question here", correctAnswer: true))
+        
+        list.append(Question(questionString: "type question here", correctAnswer: true))
+        
+        list.append(Question(questionString: "type question here", correctAnswer: true))
+        
+        list.append(Question(questionString: "type question here", correctAnswer: true))
+        
+        list.append(Question(questionString: "type question here", correctAnswer: true))
+        
+        list.append(Question(questionString: "type question here", correctAnswer: true))
+        
+        list.append(Question(questionString: "type question here", correctAnswer: true))
+        
+        list.append(Question(questionString: "type question here", correctAnswer: true))
+        
+        list.append(Question(questionString: "type question here", correctAnswer: true))
+        
+        list.append(Question(questionString: "type question here", correctAnswer: true))
+    }
+}
